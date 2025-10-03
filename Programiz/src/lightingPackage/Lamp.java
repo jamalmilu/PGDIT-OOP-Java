87f2c1d4982b6985c22b72e6312973501fb66152
jamalmilu <jamalmilu@gmail.com>
2025-10-03 09:29:52 +0600

    Lamp class

---
+++ Programiz/src/lightingPackage/Lamp.java
@@ -0,0 +1,21 @@
+package lightingPackage;
+
+public class Lamp {
+    // stores the value for light
+    // true if light is on
+    // false if light is off
+    boolean isOn;
+
+    // method to turn on the light
+    void turnOn(){
+        isOn = true;
+        System.out.println("Light on? " + isOn);
+    }
+
+    // method to turnoff the light
+    void turnoff(){
+        isOn = false;
+        System.out.println("Light on? "+ isOn);
+    }
+}
+
